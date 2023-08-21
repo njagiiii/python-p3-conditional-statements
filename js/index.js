2527d320bb8d45a623ec0a9e212f81e3c8e04314
njagiiii <grace.makena@student.moringaschool.com>
2023-08-21 01:44:17 -0400

    completed assignment

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@
   return "Access granted". Otherwise, return "Access denied".
 */
 function adminLogin(username, password) {
-  if ((username === "admin" || username === "ADMIN") && password === "12345") {
+  if ((username == "admin" || username == "ADMIN") && password == "12345") {
     return "Access granted";
   } else {
     return "Access denied";
